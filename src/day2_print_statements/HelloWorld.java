f469aae4986fa89fb0bd7c606619d1a82daacd3c
AytanMovlamova <aytan.movlamova@gmail.com>
2023-02-27 21:24:33 -0500

    Commit second time.

--- src/day2_print_statements/HelloWorld.java
+++ src/day2_print_statements/HelloWorld.java
@@ -1,15 +1,24 @@
 package day2_print_statements;
 
 public class HelloWorld {
+    public static void main (String [] args) {
 
-    public static void main (String[] args){
+        System.out.println("Hello Nadir");
 
-        System.out.println("My name is Aytan");
     }
-
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/day3_comments_escape_sequence/PrintVsPrintln.java
+++ src/day3_comments_escape_sequence/PrintVsPrintln.java
@@ -3,15 +3,19 @@ package day3_comments_escape_sequence;
 public class PrintVsPrintln {
 
     public static void main(String[] args) {
+
         System.out.println("apple");
-        System.out.print("orange");
-        System.out.print("apple");
-        System.out.print("");
-        System.out.print("alma");
+        System.out.println("banana");
+        System.out.println();
+        System.out.println("pear");
+
+
+        System.out.print("orange ");
+        System.out.println("apple");
+
+
 
 
     }
 
-
 }
-
